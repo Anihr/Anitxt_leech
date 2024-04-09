241149d987fdf468f37dafdb1a82f1c0310df46f
Anihr <151598461+Anihr@users.noreply.github.com>
2024-04-09 15:45:53 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "28982573"
 api_hash = "aee939a0fddb972d443b8e98c383ea1f"
-bot_token = "7001564218:AAHgValTyIg1AxzEOilOYfNHD3r70djG4qY"
+bot_token = "7001564218:AAHH3xpP-wU_dS00k3-J9og1QffRAUAmaqU"
